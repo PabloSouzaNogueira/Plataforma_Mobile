1e86e7f34691c37acf20b9c6bc1e37ee82f9eccb
PabloSouzaNogueira <pablo.souza18@hotmail.com>
2018-08-23 21:27:01 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+//npm install connect server-static
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname))
+    .listen(8080, function () {
+        console.log('Servidor em porta 8080...');
+    });
+
+
+    
